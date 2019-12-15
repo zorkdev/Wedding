38cbd9a756b1ec366c82ce92fe2955b263adc2dd
Attila Nemet <me@attilanemet.com>
2019-12-15 22:29:54 +0000

    Updates

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -122,11 +122,11 @@
                 minutes: 0,
                 seconds: 0,
                 words: {
-                    days: 'day',
-                    hours: 'hour',
-                    minutes: 'minute',
-                    seconds: 'second',
-                    pluralLetter: 's'
+                    days: 'nap',
+                    hours: 'óra',
+                    minutes: 'perc',
+                    seconds: 'mp',
+                    pluralLetter: ''
                 },
                 plural: true,
                 inline: false,
